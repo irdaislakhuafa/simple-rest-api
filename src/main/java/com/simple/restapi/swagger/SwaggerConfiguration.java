6e6964d03632808297795d4315282d42366eeb73
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2021-09-17 23:57:46 +0700

    Add new ApiInfo/Property on Simple Rest API Documentation

--- src/main/java/com/simple/restapi/swagger/SwaggerConfiguration.java
+++ src/main/java/com/simple/restapi/swagger/SwaggerConfiguration.java
@@ -4,19 +4,41 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Collections;
+
 @Configuration
 @EnableSwagger2
 public class SwaggerConfiguration {
+//    access Json API documentation
+//    http://localhost:8080/v2-api-docs
+
+//    using Swagger UI
+//    http://localhost:8080/swagger-ui/#/
     @Bean
     public Docket apiDocs() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(new ApiInfo(
+                        "My Simple Rest API Documentation",
+                        "This is description of Simple Rest API",
+                        "v0.1",
+                        "https://term_of_service.com",
+                        new Contact(
+                                "Irda Islakhu Afa", "https://my.domain.com", "irdhaislakhuafa@gmail.com"
+                        ),
+                        "Apache License",
+                        "https://www.apache.org/licenses/LICENSE-2.0",
+                        Collections.emptyList()
+                        )
+                );
     }
 }
