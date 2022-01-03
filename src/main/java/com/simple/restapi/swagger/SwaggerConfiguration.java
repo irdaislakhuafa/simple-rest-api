78ee7df718f26d44336b077f3c1c8fa0e7ec275e
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-01-04 02:15:17 +0700

    fix some problems

--- src/main/java/com/simple/restapi/swagger/SwaggerConfiguration.java
+++ src/main/java/com/simple/restapi/swagger/SwaggerConfiguration.java
@@ -15,30 +15,29 @@ import java.util.Collections;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfiguration {
-//    access Json API documentation
-//    http://localhost:8080/v2-api-docs
+        // access Json API documentation
+        // http://localhost:8080/v2-api-docs
 
-//    using Swagger UI
-//    http://localhost:8080/swagger-ui/#/
-    @Bean
-    public Docket apiDocs() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.simple.restapi.controller"))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(new ApiInfo(
-                        "My Simple Rest API Documentation",
-                        "This is description of Simple Rest API",
-                        "v0.1",
-                        "https://term_of_service.com",
-                        new Contact(
-                                "Irda Islakhu Afa", "https://my.domain.com", "irdhaislakhuafa@gmail.com"
-                        ),
-                        "Apache License",
-                        "https://www.apache.org/licenses/LICENSE-2.0",
-                        Collections.emptyList()
-                        )
-                );
-    }
+        // using Swagger UI
+        // http://localhost:8080/swagger-ui/#/
+        @Bean
+        public Docket apiDocs() {
+                return new Docket(DocumentationType.SWAGGER_2)
+                                .select()
+                                .apis(RequestHandlerSelectors.basePackage("com.simple.restapi.controller"))
+                                .paths(PathSelectors.any())
+                                .build()
+                                .pathMapping("/docs")
+                                .apiInfo(new ApiInfo(
+                                                "My Simple Rest API Documentation",
+                                                "This is description of Simple Rest API",
+                                                "v0.1",
+                                                "https://term_of_service.com",
+                                                new Contact(
+                                                                "Irda Islakhu Afa", "https://my.domain.com",
+                                                                "irdhaislakhuafa@gmail.com"),
+                                                "Apache License",
+                                                "https://www.apache.org/licenses/LICENSE-2.0",
+                                                Collections.emptyList()));
+        }
 }
